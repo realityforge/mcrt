af83d3379c8e51dc89d86cb152e58c40ddf87037
Peter Donald <peter@realityforge.org>
2018-03-28 14:02:09 +1100

    Add methods for defining publish tasks in Buildr

--- lib/mcrt.rb
+++ lib/mcrt.rb
@@ -5,6 +5,60 @@ require 'json'
 
 class MavenCentralPublishTool
   class << self
+    def define_publish_tasks(options = {})
+      desc 'Publish release on maven central'
+      task 'mcrt:publish' do
+        project = options[:project] || Buildr.projects[0].root_project
+        profile_name = options[:profile_name] || (raise ':profile_name not specified when defining tasks')
+        username = options[:username] || (raise ':username name not specified when defining tasks')
+        password = options[:password] || ENV['MAVEN_CENTRAL_PASSWORD'] || (raise "Unable to locate environment variable with name 'MAVEN_CENTRAL_PASSWORD'")
+        MavenCentralPublishTool.buildr_release(project, profile_name, username, password)
+      end
+
+      desc 'Publish release to maven central iff current HEAD is a tag'
+      task 'mcrt:publish_if_tagged' do
+        tag = MavenCentralPublishTool.get_head_tag_if_any
+        if tag.nil?
+          puts 'Current HEAD is not a tag. Skipping publish step.'
+        else
+          puts "Current HEAD is a tag: #{tag}"
+          if MavenCentralPublishTool.is_tag_on_candidate_branches?(tag, candidate_branches)
+            task('mcrt:publish').invoke
+          end
+        end
+      end
+    end
+
+    def get_head_tag_if_any
+      version = `git describe --exact-match --tags 2>&1`
+      if 0 == $?.exitstatus && version =~ /^v[0-9]/ && (ENV['TRAVIS_BUILD_ID'].nil? || ENV['TRAVIS_TAG'].to_s != '')
+        version.strip
+      else
+        nil
+      end
+    end
+
+    def is_tag_on_branch?(tag, branch)
+      output = `git tag --merged #{branch} 2>&1`
+      tags = output.split
+      tags.include?(tag)
+    end
+
+    def is_tag_on_candidate_branches?(tag, branches)
+      sh 'git fetch origin'
+      branches.each do |branch|
+        if is_tag_on_branch?(tag, branch)
+          puts "Tag #{tag} is on branch: #{branch}"
+          return true
+        elsif is_tag_on_branch?(tag, "origin/#{branch}")
+          puts "Tag #{tag} is on branch: origin/#{branch}"
+          return true
+        else
+          puts "Tag #{tag} is not on branches: #{branch} or origin/#{branch}"
+        end
+      end
+      false
+    end
 
     def buildr_release(project, profile_name, username, password)
       release_to_url = Buildr.repositories.release_to[:url]
